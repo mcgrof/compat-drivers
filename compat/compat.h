4f17e2abd6d313b53aa2cea6ac49de72ea7fe40a
Luis R. Rodriguez <lrodriguez@atheros.com>
2009-01-30 08:10:44 -0800

    Adds WARN_ONCE
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- compat/compat.h
+++ compat/compat.h
@@ -14,6 +14,18 @@
 
 #include <linux/pci.h>
 
+#ifndef WARN_ONCE
+#define WARN_ONCE(condition, format...) ({                      \
+	static int __warned;                                    \
+	int __ret_warn_once = !!(condition);                    \
+								\
+	if (unlikely(__ret_warn_once))                          \
+		if (WARN(!__warned, format))                    \
+			__warned = 1;                           \
+	unlikely(__ret_warn_once);                              \
+})
+#endif /* From include/asm-generic/bug.h */
+
 void __iomem *pci_ioremap_bar(struct pci_dev *pdev, int bar);
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,28)) */
