26671b84bee18f7edb93cf5e07cdddd9f52b142a
Luis R. Rodriguez <lrodriguez@atheros.com>
2009-03-24 18:32:26 -0700

    Turns out AR9170 only works on >= 2.6.29..
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- config.mk
+++ config.mk
@@ -259,9 +259,9 @@ CONFIG_RTL8187=m
 
 CONFIG_AT76C50X_USB=m
 
-# Activate AR9170 support only on kernel >= 2.6.28.
+# Activate AR9170 support only on kernel >= 2.6.29.
 # The needed USB poison feature was added in this kernel release.
-ifeq ($(shell test $(KERNEL_SUBLEVEL) -ge 28 && echo yes),yes)
+ifeq ($(shell test $(KERNEL_SUBLEVEL) -ge 29 && echo yes),yes)
 CONFIG_AR9170_COMMON=m
 CONFIG_AR9170_USB=m
 CONFIG_AR9170_LEDS=y
