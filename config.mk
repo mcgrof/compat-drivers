0562fcdb24fc3d9a9d70bb9202b51e67a4cba9ab
Luis R. Rodriguez <lrodriguez@atheros.com>
2009-08-04 14:54:32 -0700

    Disable MAC80211_MESH on config.mk
    Its busted on wireless-testing.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- config.mk
+++ config.mk
@@ -99,7 +99,8 @@ CONFIG_MAC80211_RC_MINSTREL=y
 CONFIG_MAC80211_LEDS=y
 
 # enable mesh networking too
-CONFIG_MAC80211_MESH=y
+# Mesh is currently busted on wireless-testing
+#CONFIG_MAC80211_MESH=y
 
 CONFIG_CFG80211=m
 CONFIG_CFG80211_DEFAULT_PS=y
