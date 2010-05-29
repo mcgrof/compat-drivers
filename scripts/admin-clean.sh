0ee5da33bca3eaaf45856c31c403c5274bfa97b4
Hauke Mehrtens <hauke@hauke-m.de>
2010-05-29 15:35:05 +0200

    compat-wireless: update clean scripts
    Without this patch make fails after calling make clean because compat_*
    files were deleted.
    
    Delete all files generated by scripts/admin-update.sh in
    scripts/admin-clean.sh and not in make clean
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- scripts/admin-clean.sh
+++ scripts/admin-clean.sh
@@ -2,12 +2,14 @@
 if [ -d net ] ; then
 	make clean
 fi
-rm -rf net drivers include
+rm -rf net
 rm -rf drivers
 rm -rf include
-rm -f Module.symvers
-rm -f git-describe
+rm -rf compat
+rm -rf udev
 rm -f master-tag
-rm -f compat-git-release
-rm -f compat-release
+rm -f compat_base_tree
+rm -f compat_base_tree_version
+rm -f compat_version
+rm -f code-metrics.txt
 echo "Cleaned wireless-bt-compat-2.6"
