1825131288fc1db1eedb650e1847fa5c64bc81c5
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-10-14 21:05:28 -0700

    compat-wireless: fix patches for new wl1251 changes
    wl1251 gets its new directory now.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- scripts/admin-update.sh
+++ scripts/admin-update.sh
@@ -231,6 +231,7 @@ DRIVERS="$DRIVERS drivers/net/wireless/rtl818x"
 DRIVERS="$DRIVERS drivers/net/wireless/libertas_tf"
 DRIVERS="$DRIVERS drivers/net/wireless/ipw2x00"
 DRIVERS="$DRIVERS drivers/net/wireless/wl12xx"
+DRIVERS="$DRIVERS drivers/net/wireless/wl1251"
 DRIVERS="$DRIVERS drivers/net/wireless/iwmc3200wifi"
 DRIVERS="$DRIVERS drivers/net/wireless/orinoco"
 
