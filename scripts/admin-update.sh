079861e3314936790f26f7be9a4563e7848cd486
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-11-05 15:17:46 -0700

    compat-wireless: support ignoring some stable linux-next patches
    Right before and during the merge window the amount of stable
    patches queue up on the linux-next.git tree gets pretty big
    and some of these patches may not apply cleanly on the existing
    stable release of the kernel so you may want to ignore them
    and backport them manually. Add support for this by allowing
    the packager to specify which patches should be ignored through
    the pending-stable/.ignore file.
    
    You can stuff your backported patch instead then into
    pending-stable/backport/ manually.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- scripts/admin-update.sh
+++ scripts/admin-update.sh
@@ -150,6 +150,7 @@ if [ $# -ge 1 ]; then
 		if [[ "$1" = "-s" ]]; then
 			GET_STABLE_PENDING="y"
 			EXTRA_PATCHES="${EXTRA_PATCHES} pending-stable" 
+			EXTRA_PATCHES="${EXTRA_PATCHES} pending-stable/backports/"
 			POSTFIX_RELEASE_TAG="${POSTFIX_RELEASE_TAG}s"
 			shift; continue;
 		fi
@@ -449,6 +450,12 @@ if [[ "$GET_STABLE_PENDING" = y ]]; then
 	echo -e "${GREEN}Purging old stable cherry picks... ${NORMAL}"
 	rm -f ${LAST_DIR}/${PENDING_STABLE_DIR}/*.patch
 	cp ${PENDING_STABLE_DIR}/*.patch ${LAST_DIR}/${PENDING_STABLE_DIR}/
+	if [ -f ${LAST_DIR}/${PENDING_STABLE_DIR}/.ignore ]; then
+		for i in $(cat ${LAST_DIR}/${PENDING_STABLE_DIR}/.ignore) ; do
+			echo -e "Skipping $i from generated stable patches..."
+			rm -f ${LAST_DIR}/${PENDING_STABLE_DIR}/*$i*
+		done
+	fi
 	echo -e "${GREEN}Updated stable cherry picks, review with git diff and update hunks with ./scripts/admin-update.sh -s refresh${NORMAL}"
 	cd $LAST_DIR
 fi
